1a5ac0b4dd2ca682d0b6aef15624a26135c209ca
carn181 <carn181@proton.me>
2026-02-07 22:15:03 -0500

    Backend skeleton

---
+++ backend/app/database.py
@@ -0,0 +1,10 @@
+from databases import Database
+from sqlalchemy import create_engine, MetaData
+from app.core.config import settings
+
+DATABASE_URL = settings.database_url
+
+database = Database(DATABASE_URL)
+metadata = MetaData()
+
+engine = create_engine(DATABASE_URL)
